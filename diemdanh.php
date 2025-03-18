484a6113288980cde533a75f37d7295705e28ccb
Thanh Mai <thanhmaisms111@gmail.com>
2025-03-18 18:41:53 +0700

    Cập nhật dự án

--- diemdanh.php
+++ diemdanh.php
@@ -91,6 +91,7 @@ $conn->close();
                 </ul>
             </div>
             <div class="button-container">
+                <a href="khuonmat.php" class="vertical-button">Khuôn mặt</a>
                 <button type="button" class="vertical-button" onclick="xacNhanDiemDanh()">Xác nhận</button>
                 <button type="button" class="vertical-button" id="open-recognition-btn">Bắt Đầu Nhận Diện</button>
 
